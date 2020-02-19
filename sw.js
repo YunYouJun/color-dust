04b9016017d968e54e6cfb84a9cd5444f4ed7316
YunYouJun <YunYouJun@users.noreply.github.com>
2020-02-19 18:57:38 +0000

    deploy: 620098cdd64f872eec382849953be607a0df917c

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/color-dust/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/color-dust/'), new workbox.strategies.NetworkFirst ({}), 'GET')
